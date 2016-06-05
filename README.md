408f25025f091b6264c6812e48bdbac1699b6bcd
Tom Legens <tlegens@gmail.com>
2016-06-04 21:48:36 -0500

    update readme, add calls to TGM info to functions.php

--- README.md
+++ README.md
@@ -16,13 +16,19 @@ Infinity includes all of the great features that are found in the Foundation fra
 - Translation Support
 - Bower and Gulp Support
 - Auto inclusion of SASS files in directories via sass-glob
+- Add theme dependencies
+- TGM plugin activation
+- Related posts by tags functionality
+- Much more, take a peak!
 
 ### What tools do I need to use Infinity?
-You can use whatever you want – seriously. While the Sass version comes with Bower and Gulp support out of the box, you aren’t required to use those by any means. You can use CodeKit, Grunt, Compass or nothing at all. It’s completely up to you how you decide to build you theme – Infinity won’t get in the way of your workflow.
+You can use whatever you want. While the theme does include Bower and Gulp support out of the box, you aren’t required to use those tools. You can use CodeKit, Grunt, Compass or nothing at all. It’s completely up to you how you decide to build you theme – Infinity won’t get in the way of your workflow.
 
 ### Getting Started With Gulp
 - Install [node.js](https://nodejs.org).
 - Using the command line, navigate to your theme directory
-- Run npm install
+- Run npm install (Depending on your setup, Mac users may need to run sudo npm install)
+- May need to run npm install gulp-sass-glob --save-dev for gulp-sass-glob. [Reference gulp-sass-glob ](https://www.npmjs.com/package/gulp-sass-glob)
 - Run gulp to confirm everything is working
+- Run gulp watch to have gulp watch your css and js files for changes
 
--- functions.php
+++ functions.php
@@ -1,42 +1,53 @@
 <?php
 // Theme support options
-require_once(get_template_directory().'/assets/functions/theme-support.php'); 
+require_once(get_template_directory().'/assets/functions/theme-support.php');
+
+// Theme support options
+require_once(get_template_directory().'/assets/functions/theme-helpers.php');
+
+// Bring in required and recommended plugins
+//require_once($_path.'/assets/functions/class-tgm-plugin-activation.php');
+
+// Bring in required and recommended plugins
+//require_once($_path.'/assets/functions/cms-theme-dependencies.php');
 
 // WP Head and other cleanup functions
-require_once(get_template_directory().'/assets/functions/cleanup.php'); 
+require_once(get_template_directory().'/assets/functions/cleanup.php');
 
 // Register scripts and stylesheets
-require_once(get_template_directory().'/assets/functions/enqueue-scripts.php'); 
+require_once(get_template_directory().'/assets/functions/enqueue-scripts.php');
 
 // Register custom menus and menu walkers
-require_once(get_template_directory().'/assets/functions/menu.php'); 
+require_once(get_template_directory().'/assets/functions/menu.php');
 
 // Register sidebars/widget areas
-require_once(get_template_directory().'/assets/functions/sidebar.php'); 
+require_once(get_template_directory().'/assets/functions/sidebar.php');
 
 // Makes WordPress comments suck less
-require_once(get_template_directory().'/assets/functions/comments.php'); 
+require_once(get_template_directory().'/assets/functions/comments.php');
 
 // Replace 'older/newer' post links with numbered navigation
-require_once(get_template_directory().'/assets/functions/page-navi.php'); 
+require_once(get_template_directory().'/assets/functions/page-navi.php');
 
 // Adds support for multiple languages
-require_once(get_template_directory().'/assets/translation/translation.php'); 
+require_once(get_template_directory().'/assets/translation/translation.php');
 
 // Remove 4.2 Emoji Support
-// require_once(get_template_directory().'/assets/functions/disable-emoji.php'); 
+require_once(get_template_directory().'/assets/functions/disable-emoji.php');
 
 // Adds site styles to the WordPress editor
-//require_once(get_template_directory().'/assets/functions/editor-styles.php'); 
+//require_once(get_template_directory().'/assets/functions/editor-styles.php');
 
 // Related post function - no need to rely on plugins
-// require_once(get_template_directory().'/assets/functions/related-posts.php'); 
+// require_once(get_template_directory().'/assets/functions/related-posts.php');
 
 // Use this as a template for custom post types
 // require_once(get_template_directory().'/assets/functions/custom-post-type.php');
 
 // Customize the WordPress login menu
-// require_once(get_template_directory().'/assets/functions/login.php'); 
+// require_once(get_template_directory().'/assets/functions/login.php');
 
 // Customize the WordPress admin
-// require_once(get_template_directory().'/assets/functions/admin.php'); 
+// require_once(get_template_directory().'/assets/functions/admin.php');
+
+/* Begin custom functions */
